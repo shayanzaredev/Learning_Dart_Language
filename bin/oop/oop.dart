cc5ecef4e5987ba35087896a6d460037919f2259
shayan <shayanzare2222@gmail.com>
2023-05-24 15:14:53 +0330

    source update

---
+++ bin/oop/oop.dart
@@ -0,0 +1,16 @@
+void main(){
+
+  var userr = user();
+
+
+userr.shayan = "shayan";
+userr2.shayan2 = "shayan2";
+userr = userr2;
+}
+
+class user{
+  String? shayan;
+}
+// class user2{
+//   String? shayan2;
+// }
